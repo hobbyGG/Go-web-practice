45feb268b88faf28cf30e36d82642e8e92d438a8
hobbyGG <hobbyGG@outlook.com>
2024-02-20 13:49:02 +0800

    last

--- web-tutorial/main.go
+++ web-tutorial/main.go
@@ -2,10 +2,41 @@ package main
 
 import "net/http"
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
-	})
+type helloHandler struct{}
+type aboutHandler struct{}
 
-	http.ListenAndServe("localhost:8080", nil) //defaultServeMux默认路由
+func (m *helloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world"))
+}
+
+func (a *aboutHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("About me!"))
+}
+
+func welcom(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome!"))
+}
+
+func main() {
+
+	//http.ListenAndServe的底层操作
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: nil, //DefaultServeMux
+	}
+
+	//网页处理
+	mh := &helloHandler{}
+	a := &aboutHandler{}
+	http.Handle("/hello", mh)
+	http.Handle("/about", a)
+	//Handle
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Home."))
+	})
+	http.HandleFunc("/welcome", http.HandlerFunc(welcom)) //可以直接传welcome，此为底层
+	//开始监听
+	server.ListenAndServe()
+
+	// http.ListenAndServe("localhost:8080", nil) //defaultServeMux默认路由
 }
